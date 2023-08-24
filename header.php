22be025e1bd3d45468d698877bb39ad4ae7f0505
Fazle Bari <fazlebarisn@gmail.com>
2023-08-24 22:27:00 +0600

    add here section

--- header.php
+++ header.php
@@ -53,16 +53,5 @@
   </header><!-- End Header -->
 
   <!-- ======= Hero Section ======= -->
-  <section id="hero" class="d-flex align-items-center">
-
-    <div class="container" data-aos="zoom-out" data-aos-delay="100">
-      <div class="row">
-        <div class="col-xl-6">
-          <h1>Bettter digital experience with Presento</h1>
-          <h2>We are team of talented designers making websites with Bootstrap</h2>
-          <a href="#about" class="btn-get-started scrollto">Get Started</a>
-        </div>
-      </div>
-    </div>
-
-  </section><!-- End Hero -->
+  <?php get_template_part( "template-parts/header/hero" ) ?>
+  <!-- End Hero -->
