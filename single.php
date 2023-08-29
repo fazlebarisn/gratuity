75d2c6aebdcd2c204617acec81192d3d206b5968
Fazle Bari <fazlebarisn@gmail.com>
2023-08-29 15:38:48 +0600

    display author image

--- single.php
+++ single.php
@@ -15,22 +15,32 @@ get_header();
         <?php 
             if( have_posts() ) : 
               while( have_posts() ) : the_post();
+              // dd(get_the_author_meta('description'));
                 get_template_part( 'template-parts/content' );
               endwhile;
             endif; 
         ?>
 
       <div class="blog-author d-flex align-items-center">
-        <img src="assets/img/blog/blog-author.jpg" class="rounded-circle float-left" alt="">
+        <!-- <img src="assets/img/blog/blog-author.jpg" class="rounded-circle float-left" alt=""> -->
+       <?php 
+        echo get_avatar( 
+          get_the_author_meta('user_email'), 
+          '120',
+           '',
+           'Author Image', 
+           ['class' => 'rounded-circle float-left']
+        );
+       ?>
         <div>
-          <h4>Jane Smith</h4>
+          <h4><?php echo esc_html(get_the_author());?></h4>
           <div class="social-links">
             <a href="https://twitters.com/#"><i class="bi bi-twitter"></i></a>
             <a href="https://facebook.com/#"><i class="bi bi-facebook"></i></a>
             <a href="https://instagram.com/#"><i class="biu bi-instagram"></i></a>
           </div>
           <p>
-            Itaque quidem optio quia voluptatibus dolorem dolor. Modi eum sed possimus accusantium. Quas repellat voluptatem officia numquam sint aspernatur voluptas. Esse et accusantium ut unde voluptas.
+            <?php echo esc_html( get_the_author_meta('description') ); ?>
           </p>
         </div>
       </div><!-- End blog author bio -->
