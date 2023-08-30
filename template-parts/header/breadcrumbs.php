7996a05300da7c878a2c9b8e673f79dd9b4a2aad
Fazle Bari <fazlebarisn@gmail.com>
2023-08-30 17:11:42 +0600

    working

--- template-parts/header/breadcrumbs.php
+++ template-parts/header/breadcrumbs.php
@@ -8,11 +8,27 @@
 <!-- ======= Breadcrumbs ======= -->
 <section class="breadcrumbs">
     <div class="container">
-        <ol>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="blog.html">Blog</a></li>
-            <li>Blog Single</li>
-        </ol>
-        <h2>Blog Blog</h2>
+        <?php 
+            $homepage = get_home_url();
+            $blog_page_id = get_option('page_for_posts');
+            $blog_page_url = get_permalink($blog_page_id);
+            if(is_home() ):
+        ?>
+            <ol>
+                <li><a href="<?php echo esc_url($homepage); ?>"><?php echo esc_html(__('Home', 'gratuity')); ?></a></li>
+                <li><a href="<?php echo esc_url($blog_page_url); ?>"><?php echo esc_html(__('Blog', 'gratuity')); ?></a></li>
+            </ol>
+            <h2>Blog</h2>
+        <?php elseif( is_single() ) : ?>
+            <ol>
+                <li><a href="<?php echo esc_url($homepage); ?>"><?php echo esc_html(__('Home', 'gratuity')); ?></a></li>
+                <li><a href="<?php echo esc_url($blog_page_url); ?>"><?php echo esc_html(__('Blog', 'gratuity')); ?></a></li>
+                <li><?php echo esc_html( get_the_title() ); ?></li>
+            </ol>
+            <h2>Blog</h2>
+            <?php else : ?>
+        <?php endif; ?>
     </div>
 </section><!-- End Breadcrumbs -->
+
+<?php dd( is_single(), is_page()) ?>
